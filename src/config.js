1abe3dab0014e0c25d55a42a6b3ce64fea60660a
Guy Morita <guymorita@gmail.com>
2016-09-16 23:09:57 +0200

    Updated metadata

--- src/config.js
+++ src/config.js
@@ -20,16 +20,16 @@ module.exports = Object.assign({
     head: {
       titleTemplate: 'React Redux Example: %s',
       meta: [
-        {name: 'description', content: 'All the modern best practices in one example.'},
+        {name: 'description', content: 'Helping you find the best credit cards to enable free trips.'},
         {charset: 'utf-8'},
-        {property: 'og:site_name', content: 'React Redux Example'},
-        {property: 'og:image', content: 'https://react-redux.herokuapp.com/logo.jpg'},
+        {property: 'og:site_name', content: 'Credit Tiger - Easy, Free Trips'},
+        {property: 'og:image', content: 'http://thenano.agency/wp-content/uploads/2016/03/Tony-The-Tigre.jpg'},
         {property: 'og:locale', content: 'en_US'},
-        {property: 'og:title', content: 'React Redux Example'},
-        {property: 'og:description', content: 'All the modern best practices in one example.'},
+        {property: 'og:title', content: 'Credit Tiger - Easy, Free Trips'},
+        {property: 'og:description', content: 'Helping you find the best credit cards to enable free trips.'},
         {property: 'og:card', content: 'summary'},
-        {property: 'og:site', content: '@erikras'},
-        {property: 'og:creator', content: '@erikras'},
+        {property: 'og:site', content: '@guymorita'},
+        {property: 'og:creator', content: '@guymorita'},
         {property: 'og:image:width', content: '200'},
         {property: 'og:image:height', content: '200'}
       ]
