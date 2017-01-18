b6b8cdec7f7efb7bfba0672051a66540205fd2b6
Guy Morita <guymorita@gmail.com>
2017-01-19 02:21:50 +0700

    Home page to spec

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,12 +1,10 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
-import Checkbox from 'react-toolbox/lib/checkbox';
 
 @connect(
   state => (
     {
-      routes: state.routes,
-      sort: state.sort
+      routes: state.routes
     }
   ),
   null
@@ -15,7 +13,6 @@ import Checkbox from 'react-toolbox/lib/checkbox';
 export default class Welcome extends Component {
   static propTypes = {
     routes: PropTypes.object,
-    sort: PropTypes.object,
     history: PropTypes.object
   }
 
@@ -28,37 +25,41 @@ export default class Welcome extends Component {
   render() {
     const continentsWithCountries = this.props.routes.continentsWithCountries;
     const continents = Object.keys(continentsWithCountries);
-    const { sort } = this.props;
     const styles = require('./Home.scss');
+
+    const arrowImg = require('./arrow_right.png');
 
     return (
       <div className="container-fluid">
         <div className={styles.hero + ' row'}>
           <div className="col-md-12">
-            <div className={styles.heroText}>
+            <div className={styles.heroText + ' text-center'}>
               <h1>Find Cards that will get you Free Flights</h1>
             </div>
           </div>
-          <div className="col-md-2"></div>
+        </div>
+        <div className="row">
+          <div className="col-xs-0 col-sm-0 col-md-2"></div>
           {continents.map((con) => {
             return (
-              <div className="col-md-2">
-                <div className={styles.countryBox}>
-                  <h3>{con}</h3>
+              <div className="col-xs-12 col-sm-6 col-md-2">
+                <div className={styles.countryBox + ' text-center'}>
+                  <h4>{con.toUpperCase()}</h4>
+                  <div className={styles.separatorTab}></div>
                   {continentsWithCountries[con].map((cou) => {
                     return (
-                      <Checkbox
-                        checked={sort.currentCountryName === cou}
-                        label={cou}
-                        onChange={this.handleCountryClick.bind(this, cou)}
-                      />
+                      <div>
+                        <button className={styles.countryButton} onClick={
+                          this.handleCountryClick.bind(this, cou)}>{cou}</button>
+                        <img className={styles.arrowImg} src={arrowImg} />
+                      </div>
                     );
                   })}
                 </div>
               </div>
             );
           })}
-          <div className="col-md-2"></div>
+          <div className="col-xs-0 col-sm-0 col-md-2"></div>
         </div>
         <div className={styles.valueAdds + ' row'}>
           <div className="col-md-8 col-md-offset-2">
