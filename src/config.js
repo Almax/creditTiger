d270c11b727cafe32cc7fba8fd74eae9b889157b
Guy Morita <guymorita@gmail.com>
2017-01-22 13:59:07 +0700

    OG tag changes. OG Image update

--- src/config.js
+++ src/config.js
@@ -9,29 +9,34 @@ const environment = {
   }
 }[process.env.NODE_ENV || 'development'];
 
+const NAME = 'Free Travel Guy';
+const TITLE = 'Best Travel Credit Card Offers for Free Flights';
+const DESCRIPTION = `Find how to get free flights with credit card offers. We have searched every travel card and
+                      thousands of ways to redeem points to find how to get your destination for free.`;
+
 module.exports = Object.assign({
   host: process.env.HOST || 'localhost',
   port: process.env.PORT,
   apiHost: process.env.APIHOST || 'localhost',
   apiPort: process.env.APIPORT,
   app: {
-    title: 'Free Travel Guy - Helping you find the best credit cards to enable free trips.',
-    description: 'Helping you find the best credit cards to enable free trips.',
-    name: 'Free Travel Guy',
+    title: TITLE,
+    description: DESCRIPTION,
+    name: NAME,
     head: {
       meta: [
-        {name: 'description', content: 'Helping you find the best credit cards to enable free trips.'},
+        {name: 'description', content: DESCRIPTION},
         {charset: 'utf-8'},
-        {property: 'og:site_name', content: 'FreeTravelGuy - Easy, Free Trips'},
-        {property: 'og:image', content: 'http://thenano.agency/wp-content/uploads/2016/03/Tony-The-Tigre.jpg'},
+        {property: 'og:site_name', content: NAME},
+        {property: 'og:image', content: 'http://www.freetravelguy.com/free_travel_guy_share.jpg'},
         {property: 'og:locale', content: 'en_US'},
-        {property: 'og:title', content: 'FreeTravelGuy - Easy, Free Trips'},
-        {property: 'og:description', content: 'Helping you find the best credit cards to enable free trips.'},
+        {property: 'og:title', content: TITLE},
+        {property: 'og:description', content: DESCRIPTION},
         {property: 'og:card', content: 'summary'},
         {property: 'og:site', content: '@tiger'},
         {property: 'og:creator', content: '@tiger'},
-        {property: 'og:image:width', content: '200'},
-        {property: 'og:image:height', content: '200'}
+        {property: 'og:image:width', content: '1200'},
+        {property: 'og:image:height', content: '630'}
       ]
     }
   },
--- src/containers/CardComparison/CardComparison.js
+++ src/containers/CardComparison/CardComparison.js
@@ -148,7 +148,7 @@ export default class Credit extends Component {
 
     return (
       <div>
-        <Helmet title={'Best Card for Free Flights to ' + this.state.currentCountry}/>
+        <Helmet title={'Best Travel Cards for Free Flights to ' + this.state.currentCountry}/>
         <div className={styles.card_comparison + ' container-fluid'}>
           <div className="row">
             <div className={styles.filter_menu + addnFilterClass + ' col-xs-12 col-sm-3 col-md-2'}>
